62e0b9f4149baa8d82f3b9474db27ff4b0ca41f2
Enrique Mejías Álvarez <enrique.mejias@envato.com>
2023-07-20 15:55:44 -0600

    Clean up tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['*.html', './src/**/*.rs',],
+  content: ["*.html", "./src/**/*.rs"],
   theme: {
     extend: {
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
+        poppins: ["Poppins", "sans-serif"],
       },
-      colors: {
-        'yellow-header': '#ffcc03',
-      },
-      dropShadow: {
-        'header': ['-1px 1px 2px #000']
-      }
     },
   },
   plugins: [],
-}
+};
+
